2034b3795b721d42a2bf82a9bdf25530746e14f0
wangzihuang <wangzihuang@bytedance.com>
2025-06-19 21:05:58 +0800

    25 个文件产生了变更，影响了1 个 组件、3 个 函数
    增加组件「demo1」
    增加函数「testFunction」
    增加函数「node20」
    增加函数「go1」

---
+++ functions/nodejs/generateTestFunction/index.js
@@ -0,0 +1,13 @@
+/**
+ * @description 一个简单的测试云函数，用于验证云函数的基本功能
+ * @param {Object} params - 参数对象（本函数不需要参数）
+ * @param {Object} context - 上下文对象，包含应用、用户、租户等信息
+ * @param {Logger} logger - 日志记录器
+ * @return {Object} 返回一个对象，包含函数执行状态
+ */
+module.exports = async function (params, context, logger) {
+    // 记录函数开始执行的日志
+    logger.info("测试函数开始执行");
+    
+    
+}
